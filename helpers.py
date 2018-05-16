5a5001af415ed5fb02d23ca90ab9a40159b059fd
Alex Aubuchon <alex@alxe.co>
2018-05-16 15:49:50 +0200

    Actually fixed lct_cut...

--- helpers.py
+++ helpers.py
@@ -5,6 +5,6 @@
 def lct_cut(event, lct):
     """Return true if lct isn't CSC or is a neighbor"""
     return (
-        event.hit_isCSC[lct] or
+        not event.hit_isCSC[lct] or
         event.hit_neighbor[lct]
     )
